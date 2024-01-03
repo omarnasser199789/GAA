cbecbc8a1cfc6f6b389642577faaa3949d400c45
omarnasser199789 <omarnaser1997@gmail.com>
2024-01-03 13:56:13 +0300

    clean ExamsTab

--- bricks/generate_acadimiat_app/acadimiat/__brick__/acadimiat/lib/features/my_courses_feature/presentation/pages/my_course/tabs/exams_tab.dart
+++ bricks/generate_acadimiat_app/acadimiat/__brick__/acadimiat/lib/features/my_courses_feature/presentation/pages/my_course/tabs/exams_tab.dart
@@ -1,16 +1,14 @@
 import 'package:acadmiat/core/globals.dart';
 import 'package:acadmiat/core/widgets/app_bar_widget.dart';
-import 'package:acadmiat/core/widgets/custom_botton.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import '../../../../../../../Locale/locale.dart';
 import '../../../../../../../Theme/style.dart';
-  
 import '../../../../../../../exam_folder/exam_info_page.dart';
+import '../../../../../../core/util/assets_manager.dart';
 import '../../../../data/models/my_course_model.dart';
 
 class ExamsTab extends StatefulWidget {
-  const ExamsTab({Key? key,required this.quizz}) : super(key: key);
+  const ExamsTab({super.key,required this.quizz});
 final  List<Quizz> quizz;
   @override
   State<ExamsTab> createState() => _ExamsTabState();
@@ -20,7 +18,6 @@ class _ExamsTabState extends State<ExamsTab> {
   @override
   Widget build(BuildContext context) {
 
-    var locale = AppLocalizations.of(context)!;
     return  Padding(
       padding: defaultPadding,
       child: ListView.builder(
@@ -32,14 +29,9 @@ class _ExamsTabState extends State<ExamsTab> {
                 padding: const EdgeInsets.only(bottom: 10),
                 child: GestureDetector(
                   onTap: (){
-                    goTo(context, (context) =>
-                        ExamInfoPage(
-                          quizzId: widget.quizz[index]
-                              .quizzId,)
-                    );
+                    goTo(context, (context) => ExamInfoPage(quizzId: widget.quizz[index].quizzId));
                   },
                   child: Container(
-                    // height: 128,
                     decoration: BoxDecoration(
                       borderRadius: BorderRadius.circular(12),
                       color: Theme
@@ -47,20 +39,15 @@ class _ExamsTabState extends State<ExamsTab> {
                           .cardColor,
                       boxShadow: [
                         BoxShadow(
-                          color: Theme
-                              .of(context)
-                              .cardColor
-                              .withOpacity(0.1), //Colors.grey.withOpacity(0.1),
+                          color: Theme.of(context).cardColor.withOpacity(0.1), //Colors.grey.withOpacity(0.1),
                           spreadRadius: 5,
                           blurRadius: 7,
-                          offset: const Offset(
-                              0, 3), // changes position of shadow
+                          offset: const Offset(0, 3), // changes position of shadow
                         ),
                       ],
                     ),
                     child: Padding(
-                      padding: const EdgeInsets.only(
-                          left: 13, right: 13, bottom: 12, top: 12),
+                      padding: const EdgeInsets.only(left: 13, right: 13, bottom: 12, top: 12),
                       child: Row(
                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
@@ -71,23 +58,19 @@ class _ExamsTabState extends State<ExamsTab> {
                             children: [
                               Text(
                                 widget.quizz[index].quizzName,
-                                style: blackBoldTextStyle(context: context,
-                                    fontSize: 13),
+                                style: blackBoldTextStyle(context: context, fontSize: 13),
                               ),
 
                               Padding(
                                 padding: const EdgeInsets.only(top:15),
                                 child: Text(
-                                  "عدد مرات المحاولة المسموحة :  ${widget
-                                      .quizz[index].attempts}",
-                                  style: blackBoldTextStyle(context: context,
-                                      fontSize: 11),
+                                  "عدد مرات المحاولة المسموحة :  ${widget.quizz[index].attempts}", style: blackBoldTextStyle(context: context, fontSize: 11),
                                 ),
                               )
                             ],
                           ),
                           SvgPicture.asset(
-                            "assets/svgs/question.svg",
+                            ImgAssets.question,
                             width: 35,
                             color:iconsColor,
                           ),
