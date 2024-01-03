2fe0caef3830084bb0f8f3f138b5590cd1619114
omarnasser199789 <omarnaser1997@gmail.com>
2024-01-03 13:48:08 +0300

    clean AdsTab

--- bricks/generate_acadimiat_app/acadimiat/__brick__/acadimiat/lib/features/my_courses_feature/presentation/pages/my_course/tabs/ads_tab.dart
+++ bricks/generate_acadimiat_app/acadimiat/__brick__/acadimiat/lib/features/my_courses_feature/presentation/pages/my_course/tabs/ads_tab.dart
@@ -1,12 +1,11 @@
 import 'package:acadmiat/Theme/style.dart';
 import 'package:flutter/material.dart';
-  
 import '../../../../../../../Locale/locale.dart';
 import '../../../../../../../core/widgets/cached_net_work_image.dart';
 import '../../../../../../../core/widgets/custom_botton.dart';
 
 class AdsTab extends StatefulWidget {
-  const AdsTab({Key? key}) : super(key: key);
+  const AdsTab({super.key});
 
   @override
   State<AdsTab> createState() => _AdsTabState();
@@ -28,26 +27,22 @@ class _AdsTabState extends State<AdsTab> {
               padding:
               const EdgeInsets.only(top: 15),
               child: Container(
-                // height: 116,
                 decoration: BoxDecoration(
                   color:Theme.of(context).cardColor,
                   borderRadius:
                   BorderRadius.circular(15),
                   boxShadow: [
                     BoxShadow(
-                      color: Colors.grey
-                          .withOpacity(0.1),
+                      color: Colors.grey.withOpacity(0.1),
                       spreadRadius: 5,
                       blurRadius: 7,
-                      offset: Offset(0,
-                          3), // changes position of shadow
+                      offset: const Offset(0, 3), // changes position of shadow
                     ),
                   ],
                 ),
 
                 child: Padding(
-                  padding: const EdgeInsets.only(
-                      left: 9, right: 9, top: 13),
+                  padding: const EdgeInsets.only(left: 9, right: 9, top: 13),
                   child: Column(
                     mainAxisAlignment:
                     MainAxisAlignment.start,
@@ -66,22 +61,13 @@ class _AdsTabState extends State<AdsTab> {
                             borderRadius: 6,
                             height: 25,
                             fontSize: 12,
-                            textColor:
-                            Color.fromRGBO(38,
-                                96, 164, 1),
-                            color: Color.fromRGBO(
-                                38, 96, 164, 0.1),
+                            textColor: const Color.fromRGBO(38, 96, 164, 1),
+                            color: const Color.fromRGBO(38, 96, 164, 0.1),
                           ),
                           SizedBox(
                               width: 40,
                               height: 40,
-                              child:
-                              CachedNetWorkImage(
-                                borderRadius:
-                                BorderRadius
-                                    .circular(
-                                    10),
-                              )),
+                              child: CachedNetWorkImage(borderRadius: BorderRadius.circular(10))),
                         ],
                       ),
                       Padding(
@@ -92,39 +78,25 @@ class _AdsTabState extends State<AdsTab> {
                           children: [
                             Text(
                               "${locale.coach!}/د.أيمن السيد",
-                              style:
-                              blackBoldTextStyle(context: context,
-                                  fontSize:
-                                  12),
+                              style: blackBoldTextStyle(context: context, fontSize: 12),
                             ),
                           ],
                         ),
                       ),
                       Padding(
                         padding:
-                        const EdgeInsets.only(
-                            top: 5),
+                        const EdgeInsets.only(top: 5),
                         child: Text(
                           "سيتم تأجيل لقاء اليوم لساعة واحدة",
-                          style:
-                          blackBoldTextStyle(fontSize: 11, height: 2, context:context,
-
-                              // color: Theme.of(context).accentColor
-                          ),
+                          style: blackBoldTextStyle(fontSize: 11, height: 2, context:context),
                         ),
                       ),
                       Padding(
                         padding:
-                        const EdgeInsets.only(
-                            top: 5,
-                            bottom: 14),
+                        const EdgeInsets.only(top: 5, bottom: 14),
                         child: Text(
                           "25.06.2022 10:00",
-                          style:
-                          blackBoldTextStyle(
-                              fontSize: 11, height: 2, context:context,
-                              // color: Theme.of(context).accentColor
-                          ),
+                          style: blackBoldTextStyle(fontSize: 11, height: 2, context:context),
                         ),
                       ),
                     ],
