2dfbbdcc35daf3495a16330a2eabc21dc7e44807
omarnasser199789 <omarnaser1997@gmail.com>
2024-01-08 09:18:39 +0300

    fix SVG code widget

--- bricks/generate_acadimiat_app/acadimiat/__brick__/acadimiat/lib/core/widgets/svg_code_widget.dart
+++ bricks/generate_acadimiat_app/acadimiat/__brick__/acadimiat/lib/core/widgets/svg_code_widget.dart
@@ -2,41 +2,42 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
-import '../../Theme/theme_values.dart';
-
 class SvgCodeWidget extends StatefulWidget {
-  const SvgCodeWidget({Key? key, this.svg,this.code}) : super(key: key);
-  final String ? svg;
-  final String ? code;
+  const SvgCodeWidget({Key? key, this.svg, this.code}) : super(key: key);
+  final String? svg;
+  final String? code;
 
   @override
   State<SvgCodeWidget> createState() => _SvgCodeWidgetState();
 }
 
 class _SvgCodeWidgetState extends State<SvgCodeWidget> {
-  String svgCode="";
-
-  @override
-  Future<void> didChangeDependencies() async {
-    super.didChangeDependencies();
-    String  code="";
-    if(widget.svg!=null){
-        code = await rootBundle.loadString(widget.svg!);
-    }else{
-      code=widget.code!;
-    }
-    Color primaryColor = Theme.of(context).primaryColor;
-    String hexColor = primaryColor.value.toRadixString(16).substring(2); // Remove the alpha value (ff) from the color value
-    svgCode=  code.replaceAll("ec5252",hexColor);
-    if (!mounted) {
-      return;
-    }
-    setState(() {
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    return SvgPicture.string(svgCode);
+    return FutureBuilder<String>(
+      future: loadSvgCode(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return CircularProgressIndicator(); // Show a loading indicator while waiting for the code to load
+        } else if (snapshot.hasError) {
+          return Text('Error: ${snapshot.error}');
+        } else if (snapshot.hasData) {
+          return SvgPicture.string(snapshot.data!);
+        } else {
+          return Text('No SVG code available');
+        }
+      },
+    );
+  }
+
+  Future<String> loadSvgCode() async {
+    String code = widget.svg != null
+        ? await rootBundle.loadString(widget.svg!)
+        : widget.code!; // Use widget.code directly when widget.svg is null
+    Color primaryColor = Theme.of(context).primaryColor;
+    String hexColor =
+    primaryColor.value.toRadixString(16).substring(2); // Remove the alpha value (ff) from the color value
+    return code.replaceAll("ec5252", hexColor);
   }
 }
+
