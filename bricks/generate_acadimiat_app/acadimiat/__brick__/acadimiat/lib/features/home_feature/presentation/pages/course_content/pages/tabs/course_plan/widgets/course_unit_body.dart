8ed8abb76a9c57d78265fad0d8f24e0a7402ef95
omarnasser199789 <omarnaser1997@gmail.com>
2024-01-09 12:17:26 +0300

    cleaning ...

--- bricks/generate_acadimiat_app/acadimiat/__brick__/acadimiat/lib/features/home_feature/presentation/pages/course_content/pages/tabs/course_plan/widgets/course_unit_body.dart
+++ bricks/generate_acadimiat_app/acadimiat/__brick__/acadimiat/lib/features/home_feature/presentation/pages/course_content/pages/tabs/course_plan/widgets/course_unit_body.dart
@@ -5,92 +5,115 @@ import '../../../../../../../../../core/util/assets_manager.dart';
 import '../../../../../../../data/models/card_by_id_model.dart'as m;
 import 'package:flutter_svg/flutter_svg.dart';
 
+/// A widget representing the body of a course unit
 class CourseUnitBody extends StatefulWidget {
-  const CourseUnitBody({super.key,required this.materials});
+  const CourseUnitBody({
+    Key? key,
+    required this.materials,
+  }) : super(key: key);
+
   final List<m.Material> materials;
 
   @override
   State<CourseUnitBody> createState() => _CourseUnitBodyState();
 }
 
+/// The state for the CourseUnitBody widget
 class _CourseUnitBodyState extends State<CourseUnitBody> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height:  62 * widget.materials.length / 1,
+      height: (62 * widget.materials.length)/1,
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(0),
         color: Theme.of(context).scaffoldBackgroundColor.withOpacity(0.5),
       ),
       child: ListView.builder(
-          padding: EdgeInsets.zero,
-          physics: const NeverScrollableScrollPhysics(),
-          itemCount: widget.materials.length,
-          itemBuilder: (context ,index){
-            return Column(
-              children: [
-                const SizedBox(
-                  height: 14,
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 17,right: 17),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            "${lessonsNumber(index)}:  ",
-                            style: blackBoldTextStyle(context: context,
-                                fontSize: 11, color:   hintTextColor),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.only(top:8),
-                            child: Text(widget.materials[index].materialName,style: blackBoldTextStyle(fontSize: 11, context: context),),
-                          ),
+        padding: EdgeInsets.zero,
+        physics: const NeverScrollableScrollPhysics(),
+        itemCount: widget.materials.length,
+        itemBuilder: (context, index) {
+          /// Retrieve the material at the current index
+          final material = widget.materials[index];
+          /// Check if the material has contents
+          final hasMaterialContents = material.materialContents.isNotEmpty;
 
-                        ],
-                      ),
-                      if(widget.materials[index].materialContents.isNotEmpty)
+          return Column(
+            children: [
+              const SizedBox(height: 14),
+              /// Display the material information in a row
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 17),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    /// Display the lesson number and material name
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "${lessonsNumber(index)}:  ",
+                          style: blackBoldTextStyle(
+                            context: context,
+                            fontSize: 11,
+                            color: hintTextColor,
+                          ),
+                        ),
                         Padding(
-                          padding: const EdgeInsets.only(top:5),
-                          child: Row(children: [
+                          padding: const EdgeInsets.only(top: 8),
+                          child: Text(
+                            material.materialName,
+                            style: blackBoldTextStyle(
+                              fontSize: 11,
+                              context: context,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    /// If the material has contents, display the content length and clock icon
+                    if (hasMaterialContents)
+                      Padding(
+                        padding: const EdgeInsets.only(top: 5),
+                        child: Row(
+                          children: [
                             Directionality(
-                              textDirection:
-                              TextDirection.ltr,
+                              textDirection: TextDirection.ltr,
                               child: Text(
-                                formatedTime(time: widget.materials[index].materialContents[0].contentLength.toInt()),
+                                formatedTime(
+                                  time: material.materialContents[0].contentLength.toInt(),
+                                ),
                                 style: blackBoldTextStyle(
-                                    context: context, fontSize: 11),
+                                  context: context,
+                                  fontSize: 11,
+                                ),
                               ),
                             ),
-                            const SizedBox(
-                              width: 10,
-                            ),
+                            const SizedBox(width: 10),
                             SizedBox(
                               width: 17,
                               height: 17,
                               child: SvgPicture.asset(
-                                  ImgAssets.clock,
+                                ImgAssets.clock,
                               ),
                             ),
-                          ],),
+                          ],
                         ),
-                    ],
-                  ),
+                      ),
+                  ],
                 ),
-                const SizedBox(
-                  height: 14,
-                ),
-                Container(
-                  width: double.infinity,
-                  height: 0.3,
-                  color: Theme.of(context).canvasColor.withOpacity(0.5),
-                ),
-              ],
-            );
-          }),
+              ),
+              const SizedBox(height: 14),
+              /// Divider line between each material
+              Container(
+                width: double.infinity,
+                height: 0.3,
+                color: Theme.of(context).canvasColor.withOpacity(0.5),
+              ),
+            ],
+          );
+        },
+      ),
     );
   }
 }
